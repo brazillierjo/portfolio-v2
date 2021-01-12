05feaab575b67653168518f6a6c32bb600c20c88
brazillierjo <j.brazillier@gmail.com>
2021-01-12 21:50:37 +0100

    first commit

---
+++ mail.php
@@ -0,0 +1,31 @@
+<?php
+
+	$emailTo = "j.brazillier@gmail.com";
+
+	$headers = "MIME-Version: 1.0\r\n";
+	$headers .= "Content-type: text/html; charset=utf-8\r\n";
+	$headers .= "From: ".$_POST['email']."\r\n";
+
+	if (!isset($_POST['subject'])) {
+		$subject = "Contact form message";
+	} else {
+		$subject = $_POST['subject'];
+	}
+
+	reset($_POST);
+
+	$body = "";
+	$body .= "<p><b>Nom: </b>".$_POST['name']."</p>";
+	$body .= "<p><b>Email: </b>".$_POST['email']."</p>";
+	$body .= "<p><b>Sujet: </b>".$subject."</p>";
+	$body .= "<p><b>Message: </b>".$_POST['message']."</p>";
+
+	if( mail($emailTo, $subject, $body, $headers) ){
+		$mail_sent = true;
+	} else {
+		$mail_sent = false;
+	}
+	if(!isset($resp)){
+		echo json_encode($mail_sent);
+	}
+?>
