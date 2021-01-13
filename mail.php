4fc8e2460cd7b4a963a5e9f065f8c18b76aaf7c6
brazillierjo <j.brazillier@gmail.com>
2021-01-13 09:29:59 +0100

    php contact

--- mail.php
+++ mail.php
@@ -4,21 +4,21 @@
 
 	$headers = "MIME-Version: 1.0\r\n";
 	$headers .= "Content-type: text/html; charset=utf-8\r\n";
-	$headers .= "From: ".$_POST['email']."\r\n";
+	$headers .= "From: ".$_REQUEST['email']."\r\n";
 
-	if (!isset($_POST['subject'])) {
+	if (!isset($_REQUEST['subject'])) {
 		$subject = "Contact form message";
 	} else {
-		$subject = $_POST['subject'];
+		$subject = $_REQUEST['subject'];
 	}
 
-	reset($_POST);
+	reset($_REQUEST);
 
 	$body = "";
-	$body .= "<p><b>Nom: </b>".$_POST['name']."</p>";
-	$body .= "<p><b>Email: </b>".$_POST['email']."</p>";
+	$body .= "<p><b>Nom: </b>".$_REQUEST['name']."</p>";
+	$body .= "<p><b>Email: </b>".$_REQUEST['email']."</p>";
 	$body .= "<p><b>Sujet: </b>".$subject."</p>";
-	$body .= "<p><b>Message: </b>".$_POST['message']."</p>";
+	$body .= "<p><b>Message: </b>".$_REQUEST['message']."</p>";
 
 	if( mail($emailTo, $subject, $body, $headers) ){
 		$mail_sent = true;
