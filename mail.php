64e0ca4390a6247f79257d4841ae01096f5f6fde
brazillierjo <j.brazillier@gmail.com>
2021-01-13 09:51:27 +0100

    php contact

--- mail.php
+++ mail.php
@@ -1,5 +1,34 @@
 <?php
 
+$name    = $this->escape($_POST["name"]);
+$email   = $_POST["email"];
+$message = $this->escape($_POST["message"]);
+
+$to      = "j.brazillier@gmail.com";
+$from    = "contact@synergy-development.fr";
+$subject = "New message from www.synergy-development.fr";
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= 'From: '.$from.' <'.$from.'>' . "\r\n";
+$headers .= "Reply-To: $to" . "\r\n" .
+    "X-Mailer: PHP/" . phpversion();
+
+$msg = "<h2>New contact message</h2><hr>
+        <div><b>Nom:</b> $name</div>
+        <div><b>Email:</b> $email</div>
+        <br/>
+        <div><b><u>Message:</u></b> <br/> $message</div>";
+
+
+
+    mail($to,$subject,$msg,$headers);
+
+?>
+
+
+<?php/*
+
 	$emailTo = "j.brazillier@gmail.com";
 
 	$headers = "MIME-Version: 1.0\r\n";
@@ -18,12 +47,10 @@
 	$body .= "<p><b>Sujet: </b>".$subject."</p>";
 	$body .= "<p><b>Message: </b>".$_REQUEST['message']."</p>";
 
-  mail('j.brazillier@gmail.com', 'test', 'test de mail', $headers);
-
 	if( !mail($emailTo, $subject, $body, $headers) ){
 		$mail_sent = false;
 	}
 	if(!isset($resp)){
 		echo json_encode($mail_sent);
-	}
+	} */
 ?>
